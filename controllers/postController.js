fa5d0f207fd818a35229ccc67f3eda3075eee6e4
silviosalza <silviofilipposalza@gmail.com>
2023-11-10 15:32:16 +0100

    added post array as res to postContoller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,18 @@
+const postsArray = require("../db/posts.json");
+
 function index(req,res){
+    res.format({
+        html: () => {
+            res.type("html").send("<h1>Post List</h1>")
+        },
+        json: () => {
+            res.type("json").send(postsArray)
+        },
+        default: () =>{
+            res.status(406).send("Not Acceptable")
+        }
+    })
+    return;
     res.send("Post list")
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const postController = require("./controllers/posts")
+const postController = require("./controllers/postController")
 
 dotenv.config();
 
