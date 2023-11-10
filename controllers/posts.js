4862ec268e358a6eee3055df1b5c053f76138bab
silviosalza <silviofilipposalza@gmail.com>
2023-11-10 15:12:57 +0100

    post controller index added

---
+++ controllers/posts.js
@@ -0,0 +1,7 @@
+function index(req,res){
+    res.send("Post list")
+}
+
+module.exports = {
+    index,
+}
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const postController = require("./controllers/posts")
 
 dotenv.config();
 
 const app = express();
 
 app.get("/" , (req,res) => {
+    res.format({
+        text: () => {
+            res.type("text").send("Benvenuto nel mio blog!")
+        },
+        html: () => {
+            res.type("html").send("<h1>Benvenuto nel mio blog!</h1>")
+        },
+        json: () => {
+            res.type("json").send({message: "Benvenuto nel mio blog!"})
+        },
+        default: () =>{
+            res.status(406).send("Not Acceptable")
+        }
+    })
+    return;
     res.send("Funziona")
 });
+
+app.get("/posts" , postController.index)
 
 app.listen(process.env.PORT || 3000 , () =>{
     console.log(`http://localhost:${process.env.PORT}`)
